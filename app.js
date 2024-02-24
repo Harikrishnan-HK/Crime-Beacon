a13693c8b3bb02bdd0bc92e41073176ef934bdb9
Harikrishnan-HK <krishnanhari@outlook.com>
2024-02-23 21:53:43 -0500

    db conf

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const transporter = createTransport({
   },
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/crimeBeacon");
+mongoose.connect("mongodb://127.0.0.1:27017/capstoneproject");
 
 
 app.locals.userId = "";
