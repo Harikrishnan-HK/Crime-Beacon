10999fcaa8c02be966e9763c5099002e52d653a8
Harikrishnan-HK <krishnanhari@outlook.com>
2024-02-23 22:41:15 -0500

    m db

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const transporter = createTransport({
   },
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/capstoneproject', {
+mongoose.connect('mongodb://0.0.0.0:27017/capstoneproject', {
   useNewURLParser: true,
   useUnifiedTopology: true,
 },6000000)
