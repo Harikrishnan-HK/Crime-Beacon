f352f7b6e50b797ffb71267dcbb78a256d5000cf
Harikrishnan-HK <krishnanhari@outlook.com>
2024-02-23 21:51:29 -0500

    db config

--- app.js
+++ app.js
@@ -27,19 +27,8 @@ const transporter = createTransport({
   },
 });
 
-const uri = 'mongodb://127.0.0.1:27017/crimeBeacon';
+mongoose.connect("mongodb://127.0.0.1:27017/crimeBeacon");
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
 
 app.locals.userId = "";
 
